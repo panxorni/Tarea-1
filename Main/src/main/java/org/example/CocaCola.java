7045c3ae22a19b3445b4ac9a27167d882f6c3233
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-04-24 20:28:18 -0400

    agrego clase cocacola con consumir

--- Main/src/main/java/org/example/CocaCola.java
+++ Main/src/main/java/org/example/CocaCola.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class CocaCola extends Bebida{
+    public CocaCola (TipoProducto tipo){
+        super (TipoProducto.COCACOLA);
+    }
+    public String consumir(){
+        return "cocacola";
+    }
 }
