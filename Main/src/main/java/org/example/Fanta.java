e10d813330ea8e7bb9d70c46adba9ad0b9c14911
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-04-24 20:31:30 -0400

    hago lo mismo que en cocacola

--- Main/src/main/java/org/example/Fanta.java
+++ Main/src/main/java/org/example/Fanta.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Fanta extends Bebida{
+    public Fanta (TipoProducto tipo){
+        super(TipoProducto.FANTA);
+    }
+    public String consumir(){
+        return "fanta";
+    }
 }
