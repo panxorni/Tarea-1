71ade369dcac560ab204d49befaedf526f343c19
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-04-24 20:54:16 -0400

    añado los dulces

--- Main/src/main/java/org/example/Snickers.java
+++ Main/src/main/java/org/example/Snickers.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Snickers extends Dulce{
+    public Snickers(TipoProducto tipo){
+        super(TipoProducto.SNICKERS);
+    }
+    public String consumir(){
+        return "snickers";
+    }
 }
--- Main/src/main/java/org/example/Super8.java
+++ Main/src/main/java/org/example/Super8.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Super8 extends Dulce{
+    public Super8(TipoProducto tipo){
+        super(TipoProducto.SUPER8);
+    }
+    public String consumir(){
+        return "super8";
+    }
 }
