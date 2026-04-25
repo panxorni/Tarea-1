e81d91ffb5c1555c78f47e43d2a91fdb5ef9d421
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-04-24 20:56:15 -0400

    agrego sprite

--- Main/src/main/java/org/example/Sprite.java
+++ Main/src/main/java/org/example/Sprite.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Sprite extends Bebida{
+    public Sprite(TipoProducto tipo){
+        super(TipoProducto.SPRITE);
+    }
+    public String consumir() {
+        return "sprite";
+    }
 }
