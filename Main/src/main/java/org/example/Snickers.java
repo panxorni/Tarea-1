de12387d42ce2a421dda2a8e2b14da4e0cb6e2e6
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-05-01 15:35:26 -0400

    Agrego los Javadoc restantes para las clases hijas

--- Main/src/main/java/org/example/Snickers.java
+++ Main/src/main/java/org/example/Snickers.java
@@ -1,5 +1,6 @@
 package org.example;
-
+/**Esta clase es una de las que heredan de Dulce e implementa el comportamiento de consumo.
+ * En esta se devolverá el nombre snickers cuando sea llamada.*/
 public class Snickers extends Dulce{
     public Snickers(){
         super(TipoProducto.SNICKERS);
