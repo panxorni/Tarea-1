b9e3438e39e035684d962c5151f6ad4798ace87f
Gaeo24 <hr7xscan@gmail.com>
2026-05-01 13:52:27 -0400

    Se implementa clase Comprador con consumo y calculo de vuelto. Propaga exceptions

--- Main/src/main/java/org/example/Comprador.java
+++ Main/src/main/java/org/example/Comprador.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Comprador {
+    private String consumido;
+    private int vuelto;
+    public Comprador(Moneda m,TipoProducto tipo,Expendedor exp) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException{
+        Producto p= exp.comprarProducto(m, tipo);
+
+        consumido=p.consumir();
+
+        Moneda m_vuelto= exp.getVuelto();
+        while (m_vuelto!=null){
+            vuelto+=m_vuelto.getValor();
+            m_vuelto= exp.getVuelto();
+        }
+    }
+    public int cuantoVuelto(){
+        return vuelto;
+    }
+    public String queConsumiste(){
+        return consumido;
+    }
 }
