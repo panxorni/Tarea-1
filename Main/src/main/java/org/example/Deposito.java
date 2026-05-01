3f1cd8a7c75685ca5ed48aae1a9906af9f14fef7
Francisco Rubilar <franciscorubilarcea@gmail.com>
2026-04-30 20:57:07 -0400

    Se crea clase deposito genérica

--- Main/src/main/java/org/example/Deposito.java
+++ Main/src/main/java/org/example/Deposito.java
@@ -1,4 +1,22 @@
 package org.example;
+import java.util.ArrayList;
 
-public class Deposito {
+public class Deposito<T> {
+    private ArrayList<T> almacen;
+
+    public Deposito() {
+        almacen = new ArrayList<>();
+    }
+
+    public void addElemento(T elemento) {
+        almacen.add(elemento);
+    }
+
+    public T getElemento() {
+        if (almacen.isEmpty()) {
+            return null;
+        }
+
+        return almacen.remove(0);
+    }
 }
