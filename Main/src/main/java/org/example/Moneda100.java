ff3126ac5f872545fe39ff28be078ce333d54135
Francisco Rubilar <franciscorubilarcea@gmail.com>
2026-04-30 21:18:40 -0400

    Agrego subclases Moneda100,Moneda500, Moneda1000, Moneda1500 con su documentacion javadoc

--- Main/src/main/java/org/example/Moneda100.java
+++ Main/src/main/java/org/example/Moneda100.java
@@ -1,4 +1,24 @@
 package org.example;
 
-public class Moneda100 extends Moneda{
+/**
+ * Representa una moneda con un valor de 100.
+ */
+public class Moneda100 extends Moneda {
+
+    /**
+     * Constructor de la moneda de 100.
+     */
+    public Moneda100() {
+        super();
+    }
+
+    /**
+     * Retorna el valor específico de esta moneda.
+     *
+     * @return 100.
+     */
+    @Override
+    public int getValor() {
+        return 100;
+    }
 }
