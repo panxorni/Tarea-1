9edd1de150171741cdff12ad18b3304f1aa82ac3
Gaeo24 <hr7xscan@gmail.com>
2026-05-01 15:47:29 -0400

    feat: Se implementa la clase main, contiene los 5 tests requeridos.

--- Main/src/main/java/org/example/Main.java
+++ Main/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ Probar todos los casos para comprador
 Probar las excepciones
 Ordenar lista de monedas utilizando comparable
  */
+
 public class Main {
     public static void main(String[] args) {
         Expendedor exp= new Expendedor(3);
